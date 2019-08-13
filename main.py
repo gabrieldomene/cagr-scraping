331fb3425356071cc16929ae230a7b5848f3d2b6
Gabriel Domene <domenee.g@gmail.com>
2019-08-12 22:54:24 -0300

    Working page visits + counting

--- main.py
+++ main.py
@@ -27,27 +27,43 @@ class CrawelerCAGR():
         self.check_page()
 
     def check_page(self):
-        '''Check if page initialize with table visible'''
+        '''Check if page initialize with table visible and try to select campus'''
         table = self.driver.find_element_by_id(TABLE_ID)
-        print(table)
+        # print(table)
         try:
             self.select_campus()
         except:
             print("Can't select campus")
 
     def select_campus(self):
-        '''Get all the tables result from campus'''
+        
+        '''Iterate over the options of campus'''
         for idx, campi in enumerate(FILTER_CAMPUS):
+            # Reset next page flag
+            available_page = True
             selected_campus = campi
             selected_id = idx
-            print(selected_campus)
-            select = Select(self.driver.find_element_by_id('formBusca:selectCampus'))
-            select.select_by_visible_text('UFSC/EaD')
-            self.driver.find_element_by_id('formBusca:j_id119').click()
-            time.sleep(2)
 
+            # Find total values in page to be searched
+            select = Select(self.driver.find_element_by_id('formBusca:selectCampus'))
+            select.select_by_visible_text(selected_campus) # aqui muda a variavel selected_campus para iterar no for
+            self.driver.find_element_by_id('formBusca:j_id119').click()
+            total_results = int(self.driver.find_element_by_xpath('//*[@id="formBusca:dataTableGroup"]/span').text)
+            time.sleep(1)
+            # Catch the displayed info in tables
             self.get_tables()
-    
+            # Loop while the condition is available considering the return of the method
+            value_percen = self.calculate_percentage(total_results)
+            total = value_percen
+            while(available_page):
+                print('Searching in [{}] - [{} out of {}] - [{:.4f}% - {}%]' .format(selected_campus, idx+1, len(FILTER_CAMPUS), total, 100))
+                old = total
+                if(old + value_percen >= 100):
+                    total = 100
+                else:
+                    total = old + value_percen
+                available_page = self.next_page()
+            print('\nFINISHED [{}] \n' .format(selected_campus))
     def get_tables(self):
         '''Parse tables in the html page'''
         data_raw = self.driver.find_element_by_id('formBusca:dataTable')
@@ -58,9 +74,21 @@ class CrawelerCAGR():
         
         for row in rows:
             cells = list(row.findChildren(['td']))
-            
 
+    def next_page(self):
+        '''Check if there is another page in the current campus to be visited'''
+        time.sleep(1)
+        # Find the next page button through the xpath and select the first one (could be the second)
+        try:
+            self.driver.find_element_by_xpath("(.//td[contains(@onclick, 'fastforward')])[1]").click()
+            return True
+        except:
+            print('Reached the end of pages available')
+            return False
 
+    def calculate_percentage(self, total):
+        total_items = total
+        return (50*100)/total_items
 
 
 
